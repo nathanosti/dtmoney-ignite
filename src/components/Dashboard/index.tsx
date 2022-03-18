c9c4783e4bb81e314132714b915f1628571023c1
Nathan Osti <nathanosti.job@gmail.com>
2022-03-18 12:46:11 -0300

    ♻️ Adjust TransactionTable component folder and imports name

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 import { Container } from "./styles";
 import { Summary } from "../Summary";
-import { TransactionsTable } from "../TransctionsTable";
+import { TransactionsTable } from "../TransactionsTable";
 
 export const Dashboard: FC = () => {
   return (
