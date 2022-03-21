ee772a8eb55e67599ba67caae1563acdd2135b5e
Nathan Osti <nathanosti.job@gmail.com>
2022-03-21 12:47:39 -0300

    🔥 remove old fakeAPI

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { runServer } from "./server";
-
-runServer();
 
 ReactDOM.render(
   <React.StrictMode>
